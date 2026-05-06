93afcd6d40e978086895036f81f1d767fd8394c6
Felix <git@flx.ai>
2026-05-06 15:00:12 +0200

    release: bump version to 0.2.0

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
 
       commonArgs = {
         pname = "trec";
-        version = "0.1.0";
+        version = "0.2.0";
         src = self;
         cargoLock.lockFile = ./Cargo.lock;
         buildInputs = [
